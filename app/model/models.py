528cd4f27a40238bdbd4d8005475799e253e50e9
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-11-29 18:08:14 +0300

    creating a player model

--- app/model/models.py
+++ app/model/models.py
@@ -39,3 +39,31 @@ class Team(SQLModel, table=True):
 
     # ORM-style relationship
     tournament: Optional[Tournament] = Relationship(back_populates="teams")
+    players: List["Player"] = Relationship(back_populates="team")
+
+
+class PlayerPosition(str, Enum):
+    gk = "GK"
+    cd = "CD"
+    lb = "LB"
+    rb = "RB"
+    cm = "CM"
+    cdm = "CDM"
+    cam = "CAM"
+    lw = "LW"
+    rw = "RW"
+    cf = "CF"
+
+
+class Player(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    team_id: uuid.UUID = Field(foreign_key="team.id")
+    name: str = Field(max_length=100)
+    position: PlayerPosition
+    number: int = Field(ge=1, le=99)
+    photo_url: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+    # ORM-style relationship
+    team: Optional["Player"] = Relationship(back_populates="players")
