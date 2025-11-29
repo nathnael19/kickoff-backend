b891e0e2da4911ff6f098821211da3c7b7411d32
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-11-29 14:03:21 +0300

    creating team model and its relationship with the tournament model

--- app/model/models.py
+++ app/model/models.py
@@ -1,8 +1,8 @@
-from enum import Enum
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import List, Optional
+from sqlmodel import SQLModel, Field, Relationship
 import uuid
 from datetime import datetime
+from enum import Enum
 
 
 class TournamentStatus(str, Enum):
@@ -13,11 +13,29 @@ class TournamentStatus(str, Enum):
 
 class Tournament(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    name: str
-    description: str = Field(max_length=500)
-    status: TournamentStatus
+    name: str = Field(..., max_length=100)
+    description: Optional[str] = Field(default=None, max_length=500)
+    status: TournamentStatus = Field(default=TournamentStatus.planned)
     logo_url: Optional[str] = None
     start_date: datetime
     end_date: Optional[datetime] = None
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: Optional[datetime] = None
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+    # ORM-style relationship
+    teams: List["Team"] = Relationship(back_populates="tournament")
+
+
+class Team(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    tournament_id: uuid.UUID = Field(foreign_key="tournament.id")
+    name: str = Field(..., max_length=100)
+    description: Optional[str] = Field(default=None, max_length=500)
+    department: str = Field(..., max_length=100)
+    coach: str = Field(..., max_length=100)
+    logo_url: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+    # ORM-style relationship
+    tournament: Optional[Tournament] = Relationship(back_populates="teams")
